c1bbd70d907ae108ff61a5f1927c8d97fc0510e4
LarissaRibeiro015 <142758863+LarissaRibeiro015@users.noreply.github.com>
2023-08-20 21:14:31 -0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Meu Script Paste</title>
+    <style>
+        /* Estilos gerais ... */
+    </style>
+</head>
+<body>
+    <header>
+        <h1>LarinhaGG</h1>
+        <nav>
+            <ul>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/scripts.html">Scripts</a></li>
+                <li><a href="/executores.html">Executores</a></li>
+                <li><a href="/about.html">Sobre</a></li>
+                <li><a href="/contato.html">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
